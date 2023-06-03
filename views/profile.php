9544ec5b2cf3314655bae21d3023cebdc0cf1ac2
Virillo <albertomedvel@gmail.com>
2023-06-03 21:52:47 +0200

    cambios perfil

--- views/profile.php
+++ views/profile.php
@@ -5,24 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="libraries/bootstrap/css/bootstrap.min.css " rel="stylesheet" />
-    <link href="css/perfileStyle.css" rel="stylesheet" />
-    <script src="libraries/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link href="libraries/bootstrap/css/bootstrap.min.css " rel="stylesheet" />   
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <link href="/css/perfileStyle.css" rel="stylesheet" />
     <title>Perfil</title>
 </head>
 <body>
+
     <?php
     $nameView = "Calendario";
     include("views/header.php");
-    include("views/footer.php");
     ?>
     
-   <div class="container">
+   <div class="container center-container mt-3 ml-3">
     <div class="row">
       <div class="col-md-8">
-        <h2>Información de Perfil</h2>
+        <h2 class="text-center">Información de perfil</h2>
         <hr>
-        <form class="row g-3">
+        <form class="row g-3 mt-3 mb-3">
             <div class="col-md-6">
                 <label for="Nombre" class="form-label">Nombre:</label>
                 <input type="text" class="form-control" id="inputEmail4" readonly value="<?php echo $user['nombre']; ?>">
@@ -33,84 +35,120 @@
             </div>
             <div class="col-6">
                 <label for="turno" class="form-label">Turno:</label>
-                <input type="text" class="form-control" id="turno" value="<?php echo $user['turno'];?>">
+                <input type="text" class="form-control" id="turno" readonly value="<?php echo $user['turno'];?>">
             </div>
             <div class="col-6">
                 <label for="dedicacion" class="form-label">Dedicación:</label>
-                <input type="text" class="form-control" id="dedicacion" value="<?php echo $user['dedicacion'];?>">
+                <input type="text" class="form-control" id="dedicacion" readonly value="<?php echo $user['dedicacion'];?>">
             </div>
         </form>
       </div>
     </div>
-    <div class="row">
-        <div class="col-md-8">
-            <h2>Datos opcionales para el formulario</h2>
+    <div class="row mt-3 mb-3">
+        <div class="col-md-8 mt-3 mb-3">
+            <h2 class="text-center">Datos opcionales para el formulario</h2>
+            <i class="mt-3 mb-3">*Estos campos servirán para autocompletar el formulario al solicitar dias, estos mismos se podrán modificar en el formulario de solicitud si así se desea</i>
             <hr>
-            <form class="row g-3">
+            <form class="row g-3 mt-3 mb-3">
+              <div class="row g-3 mt-3 mb-3">
                 <div class="col-md-6">
-                    <label for="inputEmail4" class="form-label">Apellido 1</label>
-                    <input type="email" class="form-control" id="inputEmail4">
+                      <label for="inputEmail4" class="form-label">Apellido 1</label>
+                      <input type="email" class="form-control" id="inputEmail4">
+                  </div>
+                  <div class="col-md-6">
+                      <label for="inputPassword4" class="form-label">Apellido 2</label>
+                      <input type="password" class="form-control" id="inputPassword4">
+                  </div>
                 </div>
-                <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Apellido 2</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                <div class="row g-3">
+                  <div class="col-6">
+                      <label for="DNI" class="form-label">DNI</label>
+                      <input type="text" class="form-control" id="DNI" placeholder="XXXXXXXXXX">
+                  </div>
                 </div>
-                <div class="col-6">
-                    <label for="DNI" class="form-label">DNI</label>
-                    <input type="text" class="form-control" id="DNI" placeholder="XXXXXXXXXX">
+                <div class="row g-3">
+                  <div class="col-md-6">
+                      <label for="fijo" class="form-label">Teléfono fijo</label>
+                      <input type="text" class="form-control" id="fijo" placeholder="000000000">
+                  </div>
+                  <div class="col-md-6">
+                      <label for="movil" class="form-label">Teléfono movil</label>
+                      <input type="text" class="form-control" id="movil" placeholder="000000000">
+                  </div>
                 </div>
-                <div class="col-md-6">
-                    <label for="fijo" class="form-label">Teléfono fijo</label>
-                    <input type="text" class="form-control" id="fijo" placeholder="000000000">
+                <div class="row g-3"><h4 class="text-center">Información de dirección</h4></div>
+                <hr>
+                <div class="row g-3 mt-3 mb-3">
+                  <div class="col-md-2">
+                      <label for="tipo_via" class="form-label">Tipo de via</label>
+                      <select id="tipo_via" class="form-select">
+                      <option selected>Calle</option>
+                      <option>Avenida</option>
+                      <option>Plaza</option>
+                      </select>
+                  </div>
+                  <div class="col-md-9">
+                      <label for="nombre_via" class="form-label">Nombre de via</label>
+                      <input type="text" class="form-control" id="nombre_via" placeholder="Islas Canarias">
+                  </div>
+                  <div class="col-md-1">
+                    <label for="numero_via" class="form-label">Nº</label>
+                    <input type="number" class="form-control" id="numero_via">
+                  </div>
                 </div>
-                <div class="col-md-6">
-                    <label for="movil" class="form-label">Teléfono movil</label>
-                    <input type="text" class="form-control" id="movil" placeholder="000000000">
+                <div class="row g-3 d-flex justify-content-between">
+                  <div class="col-md-1">
+                      <label for="portal" class="form-label">Nº Portal</label>
+                      <input type="number" class="form-control" id="portal">
+                  </div>
+                  <div class="col-md-1">
+                      <label for="escalera" class="form-label">Nº Escalera</label>
+                      <input type="number" class="form-control" id="escalera">
+                  </div>
+                  <div class="col-md-1">
+                      <label for="puerta" class="form-label">Letra Puerta</label>
+                      <input type="text" class="form-control" id="puerta">
+                  </div>
                 </div>
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">State</label>
-                    <select id="inputState" class="form-select">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="inputZip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
+                <div class="row g-3">
+                  <div class="col-md-6">
+                      <label for="Provincia" class="form-label">Provincia</label>
+                      <input type="text" class="form-control" id="puerta">
+                  </div>
+                  <div class="col-md-6">
+                      <label for="localidad" class="form-label">Localidad</label>
+                      <input type="text" class="form-control" id="localidad">
+                  </div>
                 </div>
             </form>
         </div>
     </div>
-      <div class="col-md-8">
-        <h2>Otra información</h2>
+      <div class="col-md-8 mt-3 mb-3 text-center ">
+        <h2 class="text-center">Cambio de información</h2>
         <hr>
-        <div class="row">
+        <div class="row mt-3 mb-3 d-flex justify-content-between">
           <div class="col-sm-6">
             <h4>Cambio de contraseña</h4>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#modal1">Cambio de contraseña</button>
+            <button class="btn btn-primary mt-3 mb-3" data-toggle="modal" data-target="#modal1">Cambio de contraseña</button>
           </div>
           <div class="col-sm-6">
             <h4>Información 2</h4>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#modal2">Botón 2</button>
+            <button class="btn btn-primary mt-3 mb-3" data-toggle="modal" data-target="#modal2">Botón 2</button>
           </div>
+        </div>
+        <div class="row mt-3 mb-3 d-flex justify-content-between text-center">
           <div class="col-sm-6">
             <h4>Información 3</h4>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#modal3">Botón 3</button>
+            <button class="btn btn-primary mt-3 mb-3" data-toggle="modal" data-target="#modal3">Botón 3</button>
           </div>
           <div class="col-sm-6">
             <h4>Información 4</h4>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#modal4">Botón 4</button>
+            <button class="btn btn-primary mt-3 mb-3" data-toggle="modal" data-target="#modal4">Botón 4</button>
           </div>
         </div>
-      </div>
+            
+          </div>
+        </div>
     
   </div>
 
@@ -202,17 +240,14 @@
   </div>
 
 
-  <footer class="bg-light text-center text-lg-start" >
+
+  <footer class="bg-light text-center text-lg-start mt-3" >
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2023 
     <a class="text-dark" href="http://secretaria.iesjovellanos.org/">Secretaria IES Gaspar Melchor de Jovellanos</a>
   </div> 
 </footer>
-
-
-
-  
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+ 
 
 
 

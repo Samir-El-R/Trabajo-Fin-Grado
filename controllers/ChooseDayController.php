b734979e55d8bd0987baa441581c3d71c403c86f
cesar74545 <moathaicessar@hotmail.com>
2023-06-14 22:19:00 +0200

    samir es totalmente gay

--- controllers/ChooseDayController.php
+++ controllers/ChooseDayController.php
@@ -17,7 +17,7 @@ class ChooseDayController
         header('Location: views/chooseDays.php');
     }
 
-    public function insertarSolicitudes($correoProfesor,$data)
+    public function insertarSolicitudes($correoProfesor, $data)
     {
 
         $query = "SELECT * FROM profesores WHERE correo = '$correoProfesor'";
@@ -27,24 +27,31 @@ class ChooseDayController
 
         if ($fila) {
             $idTabla1 = $fila['id'];
-        
+
             $archivos = $this->leerDirectorios($correoProfesor);
-            for ($i = 0; $i < count($data['fecha']); $i++) {
-                $fecha = $data["fecha"][$i];
-                $pdf = $rutaDirectorio . $archivos[$i];
-                $nombreArchivo = basename($pdf); // Obtiene el nombre del archivo sin la ruta
-                
-                $insertBBDD = "INSERT INTO diasseleccionados (idProfesor, fechaEscogida, estado, solicitud) VALUES (?, ?, 'Pendiente', ?)";
-                
-                $stmt = $this->db->descriptor->prepare($insertBBDD);
-                $stmt->bind_param("iss", $idTabla1, $fecha, $pdf);
-                $stmt->execute();
-                
-                // Ejecutar la consulta preparada
-                $resultado = $stmt->get_result();
-                
-                $stmt->close();
+            foreach ($data['fecha'] as $fecha) {
+                $i = 0;
+                // $fecha = $data["fecha"][$i];
+                if ($fecha != null && $fecha != '') {
+
+                    $pdf = $rutaDirectorio . $archivos[$i];
+                    $i++;
+                    // $nombreArchivo = basename($pdf); // Obtiene el nombre del archivo sin la ruta
+
+                    $insertBBDD = "INSERT INTO diasseleccionados (idProfesor, fechaEscogida, estado, solicitud) VALUES ('$idTabla1', '$fecha', 'Pendiente', '$pdf')";
+                    $this->db->consulta($insertBBDD);
+
+                    // $stmt = $this->db->descriptor->prepare($insertBBDD);
+                    // $stmt->bind_param("iss", $idTabla1, $fecha, $pdf);
+                    // $stmt->execute();
+
+                    // // Ejecutar la consulta preparada
+                    // $resultado = $stmt->get_result();
+
+                    // $stmt->close();
+                }
             }
+
         }
     }
 
@@ -52,16 +59,16 @@ class ChooseDayController
     {
         $directorio = 'pdfDiasLibres'; // Ruta del directorio a recorrer
         $nombreUsuario = $correoProfesor; // Nombre de usuario para filtrar los archivos
-        
+
         if (!is_readable($directorio)) {
             exit;
         }
 
         $archivos = array(); // Array para almacenar los nombres de los archivos
-        
+
         // Recorrer el directorio y almacenar los archivos en el array
         if ($handle = opendir($directorio)) {
-  
+
             while (false !== ($archivo = readdir($handle))) {
 
                 if ($archivo != "." && $archivo != "..") {
